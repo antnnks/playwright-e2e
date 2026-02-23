a7166d1d8c651f1e62078d2dd242f6dbf9ba30be
antnnks <antnnks@gmail.com>
2026-02-23 18:04:13 +0100

    separated for 2 tests

---
+++ tests/anmelden.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect, _baseTest } from '@playwright/test';
+
+test('Anmelden', async ({ page }) => {
+  const email = process.env.USER_EMAIL!;
+  const password = process.env.USER_PASSWORD!;
+  await page.goto('https://www.tagesspiegel.de/');
+  await page.locator('iframe[title="Iframe title"]').contentFrame().getByRole('button', { name: 'Alle akzeptieren' }).click();
+  await page.getByRole('link', { name: 'Anmelden' }).click();
+  await page.locator('#modal-iframe').contentFrame().getByRole('textbox', { name: 'E-Mail' }).fill(email);
+  await page.locator('#modal-iframe').contentFrame().getByRole('textbox', { name: 'Passwort' }).click();
+  await page.locator('#modal-iframe').contentFrame().getByRole('textbox', { name: 'Passwort' }).fill(password);
+  await page.locator('#modal-iframe').contentFrame().getByRole('button', { name: 'Anmelden' }).click();
+  await expect(page.getByRole('button', { name: 'Mein Konto' })).toBeVisible({ timeout: 15000 });
+});
