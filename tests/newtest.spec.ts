3bba9f327e850a318eb62a439c6ebdfa2fccd94e
antnnks <antnnks@gmail.com>
2026-02-23 15:37:03 +0100

    login test and secrets

--- tests/newtest.spec.ts
+++ tests/newtest.spec.ts
@@ -1,5 +1,23 @@
 import { test, expect, _baseTest } from '@playwright/test';
 
+
+test('login', async ({ page }) => {
+  await page.goto('https://www.tagesspiegel.de/');
+  await page.locator('iframe[title="Iframe title"]').contentFrame().getByRole('button', { name: 'Alle akzeptieren' }).click();
+  await page.getByRole('link', { name: 'Anmelden' }).click();
+  await page.locator('#modal-iframe').contentFrame().getByRole('textbox', { name: 'E-Mail' }).fill(process.env.USER_EMAIL!);;
+  await page.locator('#modal-iframe').contentFrame().getByRole('textbox', { name: 'Passwort' }).click();
+  await page.locator('#modal-iframe').contentFrame().getByRole('textbox', { name: 'Passwort' }).click();
+  await page.locator('#modal-iframe').contentFrame().getByRole('textbox', { name: 'Passwort' }).fill('https://www.tagesspiegel.de/');
+  await page.locator('#modal-iframe').contentFrame().getByRole('textbox', { name: 'Passwort' }).click();
+  await page.locator('#modal-iframe').contentFrame().getByRole('textbox', { name: 'Passwort' }).fill(process.env.USER_PASSWORD!);;
+  await page.locator('#modal-iframe').contentFrame().getByRole('button', { name: 'Anmelden' }).click();
+  await expect(page.getByRole('link', { name: 'Anmelden' })).not.toBeVisible({ timeout: 10000 });
+  await expect(page.getByRole('button', { name: 'Mein Konto' })).toBeVisible({ timeout: 10000 });
+});
+
+
+
 test('search', async ({ page }) => {
   await page.goto('https://www.tagesspiegel.de/');
   await page.locator('iframe[title="Iframe title"]').contentFrame().getByRole('button', { name: 'Alle akzeptieren' }).click();
